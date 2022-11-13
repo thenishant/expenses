cbf804fd6053aa21e082d5914734aeba13bfa42a
||thenishant|| <thenishant3@gmail.com>
2022-11-13 23:02:30 +0700

    Add expense specific route

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const bodyParser = require('body-parser')
 const expenseRouter = require('./routes/expenses')
 const app = express();
 
-app.use(expenseRouter)
+app.use('/api/expense', expenseRouter)
 
 app.listen(process.env.PORT)
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,10 +1,25 @@
 const express = require('express')
 
 const router = express.Router()
+const date = new Date();
 
-router.get('/', (req, res, next) => {
-    console.log("GET request in expenses");
-    res.json({message: "It works in expenses"})
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        date: date.toLocaleDateString(),
+        day: date.getDate().toLocaleString(),
+        type: 'Grocery',
+        amount: 200,
+        desc: 'vegetables'
+    }
+]
+
+router.get('/:id', (req, res, next) => {
+    const expenseId = req.params.id;
+    const expense = DUMMY_EXPENSES.find(expense => {
+        return expense.id === expenseId
+    });
+    res.json({expense})
 })
 
 module.exports = router
