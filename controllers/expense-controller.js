a01a8fc500626d5d5f35b3297dbbbcdfc2a72fa4
nishant-qantas <nishant.sharma@qantas.com.au>
2022-11-24 21:07:19 +0700

    Add CRUD operation

--- controllers/expense-controller.js
+++ controllers/expense-controller.js
@@ -5,26 +5,22 @@ const date = new Date();
 
 const Expense = require('../models/expense')
 
-let DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        date: date.toLocaleDateString(),
-        day: date.getDate().toLocaleString(),
-        type: 'Grocery',
-        amount: 200,
-        desc: 'vegetables'
-    }
-]
-
-const getExpenseById = (req, res, next) => {
+const getExpenseById = async (req, res, next) => {
     const expenseId = req.params.id;
-    const expense = DUMMY_EXPENSES.find(expense => {
-        return expense.id === expenseId
-    });
+    let expense
+    try {
+        expense = await Expense.findById(expenseId)
+    } catch (err) {
+        const error = new HttpError("Something went wrong", 500)
+        return next(error)
+    }
 
-    if (!expense)
-        throw new HttpError('Cannot find expense with the associated id', 404)
-    res.json({expense})
+    if (!expense) {
+        const error = new HttpError('Cannot find expense with the associated id', 404)
+        return next(error)
+    }
+
+    res.json({expense: expense.toObject({getters: true})})
 }
 
 function validateRequest(request) {
@@ -46,34 +42,60 @@ const createExpense = async (request, response, next) => {
         const error = new HttpError('Creating expense failed, please try again', 500)
         return next(error)
     }
-    DUMMY_EXPENSES.push(createdExpense)
 
     response.status(201).json({expense: createdExpense})
 }
 
-const updateExpense = (req, res, next) => {
+const updateExpense = async (req, res, next) => {
     validateRequest(req)
 
     const {type, amount, desc} = req.body
 
     const expenseId = req.headers.id;
-    const updatedExpense = {...DUMMY_EXPENSES.find(e => e.id === expenseId)};
-    const expenseIndex = DUMMY_EXPENSES.findIndex(e => e.id === expenseId);
 
-    updatedExpense.type = type
-    updatedExpense.amount = amount
-    updatedExpense.desc = desc
+    let expense
+    try {
+        expense = await Expense.findById(expenseId)
+    } catch (err) {
+        const error = new HttpError("Something went wrong", 500)
+        return next(error)
+    }
 
-    DUMMY_EXPENSES[expenseIndex] = updatedExpense
+    expense.type = type
+    expense.amount = amount
+    expense.desc = desc
 
-    res.status(200).json({expense: updatedExpense})
+    try {
+        await expense.save()
+    } catch (err) {
+        console.log(err)
+        const error = new HttpError('Update failed', 500)
+        return next(error)
+    }
+
+    res.status(200).json({expense: expense.toObject({getters: true})})
 }
 
-const deleteExpense = (req, res, next) => {
+const deleteExpense = async (req, res, next) => {
     const expenseId = req.headers.id;
-    DUMMY_EXPENSES = DUMMY_EXPENSES.filter(e => e.id !== expenseId)
+    let expense
+    try {
+        expense = await Expense.findById(expenseId)
+    } catch (err) {
+        const error = new HttpError("Something went wrong", 500)
+        return next(error)
+    }
 
-    res.status(200).json({message: "Expense deleted"})
+    try {
+        await expense.remove()
+    } catch (err) {
+        console.log(err)
+        const error = new HttpError('Delete failed', 500)
+        return next(error)
+    }
+
+
+    res.status(200).json({expense: expense.toObject({getters: true})})
 }
 
 exports.getExepenseById = getExpenseById
